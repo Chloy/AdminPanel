6d082eef89d02d43c18a25d23fb57834a3156006
Dary Sagitov <krisbeaky@gmail.com>
2022-01-29 18:23:46 +0500

    Create and edit DB

--- WorkProject/WorkProject/settings.py
+++ WorkProject/WorkProject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ansible_admin_panel.apps.AnsibleAdminPanelConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ WorkProject/ansible_admin_panel/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AnsibleAdminPanelConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ansible_admin_panel'
