31b50c1e0393b0101d53a5bcbc13de6c4d6cad46
Dary Sagitov <krisbeaky@gmail.com>
2022-01-30 17:24:55 +0500

    Add templates folder and home page

--- WorkProject/ansible_admin_panel/views.py
+++ WorkProject/ansible_admin_panel/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Hella')
+    return render(request, 'ansible_admin_panel/home.html')
