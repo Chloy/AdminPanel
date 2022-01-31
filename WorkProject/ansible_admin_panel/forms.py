84c942f0abe266285d8b330c8eb2b878688656a9
Dary Sagitov <krisbeaky@gmail.com>
2022-01-31 22:41:50 +0500

    Create form prototype

--- WorkProject/ansible_admin_panel/forms.py
+++ WorkProject/ansible_admin_panel/forms.py
@@ -1,13 +1,16 @@
-from hashlib import new
 from django import forms
-import models
+from .models import ORG, Host
+
+
+class NameForm(forms.Form):
+    your_name = forms.ModelChoiceField(queryset=ORG.objects.all())
 
 class HostForm(forms.Form):
     name = forms.CharField(max_length=250)
-    org = forms.ModelChoiceField(queryset=models.ORG.objects.all())
+    org = forms.ModelChoiceField(queryset=ORG.objects.all())
 
     def save(self):
-        new_host = models.Host.objects.create(
+        new_host = Host.objects.create(
             name = self.name,
             org = self.org
         )
--- WorkProject/ansible_admin_panel/urls.py
+++ WorkProject/ansible_admin_panel/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='ansible_admin_panel_home')
+    path('', views.get_name, name='ansible_admin_panel_home')
 ]
