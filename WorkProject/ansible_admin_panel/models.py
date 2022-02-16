5882f2780091a6c1900f7721d5c3850c51db81dd
root <root@or-v-sr-deb-tml.es.efsystem.ru>
2022-02-16 17:50:15 +0500

    Edit models, add tv to Host

--- WorkProject/ansible_admin_panel/models.py
+++ WorkProject/ansible_admin_panel/models.py
@@ -1,129 +1,148 @@
 from django.db import models
-from django.shortcuts import reverse
 
 class ORG(models.Model):
     name = models.CharField(unique=True, max_length=250)
+    parent = models.ForeignKey("self", blank=True, on_delete=models.SET_NULL, null=True)
     
     def __str__(self):
         return self.name
 
 class LOCATION(models.Model):
     name = models.CharField(unique=True, max_length=250)
+    parent = models.ForeignKey("self", blank=True, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name    
 
 class HV(models.Model):
     name = models.CharField(unique=True, max_length=250)
+    parent = models.ForeignKey("self", blank=True, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
 
 class CLASS(models.Model):
     name = models.CharField(unique=True, max_length=250)
+    parent = models.ForeignKey("self", blank=True, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
 
 class ASSIGNIP(models.Model):
     name = models.CharField(unique=True, max_length=250)
+    parent = models.ForeignKey("self", blank=True, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
 
 class FAMILY(models.Model):
     name = models.CharField(unique=True, max_length=250)
+    parent = models.ForeignKey("self", blank=True, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
 
 class ROLE(models.Model):
     name = models.CharField(unique=True, max_length=250)
+    parent = models.ForeignKey("self", blank=True, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
 
 class FEATURE(models.Model):
     name = models.CharField(unique=True, max_length=250)
+    parent = models.ForeignKey("self", blank=True, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
 
 class TV(models.Model):
     name = models.CharField(unique=True, max_length=250)
+    parent = models.ForeignKey("self", blank=True, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
 
 class KNA(models.Model):
     name = models.CharField(unique=True, max_length=250)
+    parent = models.ForeignKey("self", blank=True, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
 
 class KES(models.Model):
     name = models.CharField(unique=True, max_length=250)
+    parent = models.ForeignKey("self", blank=True, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
 
 class BE(models.Model):
     name = models.CharField(unique=True, max_length=250)
+    parent = models.ForeignKey("self", blank=True, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
 
 class CON(models.Model):
     name = models.CharField(unique=True, max_length=250)
+    parent = models.ForeignKey("self", blank=True, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
 
 class AVAILABILITY(models.Model):
     name = models.CharField(unique=True, max_length=250)
-
+    parent = models.ForeignKey("self", blank=True, on_delete=models.SET_NULL, null=True)
+    
     def __str__(self):
         return self.name
 
 class ELK(models.Model):
     name = models.CharField(unique=True, max_length=250)
+    parent = models.ForeignKey("self", blank=True, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
 
 class DINET(models.Model):
     name = models.CharField(unique=True, max_length=250)
+    parent = models.ForeignKey("self", blank=True, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
 
 class WU(models.Model):
     name = models.CharField(unique=True, max_length=250)
+    parent = models.ForeignKey("self", blank=True, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
 
 class SSH(models.Model):
     name = models.CharField(unique=True, max_length=250)
-
+    parent = models.ForeignKey("self", blank=True, on_delete=models.SET_NULL, null=True)
+    
     def __str__(self):
         return self.name
 
 class LOCAL_OS(models.Model):
     name = models.CharField(unique=True, max_length=250)
-
+    parent = models.ForeignKey("self", blank=True, on_delete=models.SET_NULL, null=True)
+    
     def __str__(self):
         return self.name
 
 class STAGE(models.Model):
     name = models.CharField(unique=True, max_length=250)
+    parent = models.ForeignKey("self", blank=True, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
 
 class VarType(models.Model):
     name = models.CharField(unique=True, max_length=250)
-
+    
     def __str__(self):
         return self.name
 
@@ -135,6 +154,9 @@ class Var(models.Model):
         return "{}={}".format(self.var_type, self.value)
 
 class Host(models.Model):
+    class Meta:
+        db_table = 'wada'
+
     name = models.CharField(unique=True, max_length=250)
     org = models.ForeignKey(ORG, on_delete=models.DO_NOTHING)
     location = models.ForeignKey(LOCATION, on_delete=models.DO_NOTHING)
@@ -144,6 +166,7 @@ class Host(models.Model):
     family = models.ForeignKey(FAMILY, on_delete=models.DO_NOTHING)
     roles = models.ManyToManyField(ROLE, blank=True)
     features = models.ManyToManyField(FEATURE, blank=True)
+    tv = models.ForeignKey(TV, on_delete=models.DO_NOTHING, default=TV.objects.get(name='tv-NONE').id)
     kna = models.ForeignKey(KNA, on_delete=models.DO_NOTHING)
     kes = models.ForeignKey(KES, on_delete=models.DO_NOTHING)
     be = models.ForeignKey(BE, on_delete=models.DO_NOTHING)
@@ -156,9 +179,6 @@ class Host(models.Model):
     local_os = models.ForeignKey(LOCAL_OS, on_delete=models.DO_NOTHING)
     stage = models.ForeignKey(STAGE, on_delete=models.DO_NOTHING)
     vars = models.ManyToManyField(Var, blank=True)
-
-    def get_absolute_url(self):
-        return reverse('ansible_admin_panel_object_detail', kwargs={'slug': 'host', 'pk': self.pk})
 
     def __str__(self):
         return self.name
